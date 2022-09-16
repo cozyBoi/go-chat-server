a426044dfb7db3a8201c284717d0ced7fc9903e3
cozyBoi <kjjh1126@sogang.ac.kr>
2022-09-16 15:09:17 +0900

    add chat log collection

--- main.go
+++ main.go
@@ -1,15 +1,137 @@
 package main
+
 import (
-    "net/http"
-    "github.com/gin-gonic/gin"
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
-func main(){
-    r := gin.Default()
-    r.GET("ping", func(c *gin.Context){
-        c.JSON(http.StatusOK, gin.H{
-            "message": "pong",
-        })
-    })
-    r.Run()
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+var upgrader = websocket.Upgrader{} // use default options
+
+var chat_log []string
+
+//var lzst_prnt_idx map[websocket.Conn]int
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+
+	//print previous chats
+	for _, chat := range chat_log {
+		c.WriteMessage(1, []byte(chat))
+	}
+
+	for {
+		mt, message, err := c.ReadMessage() //get msg type and msg
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		chat_log = append(chat_log, string(message))
+		err = c.WriteMessage(mt, message) //echo
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
 }
+
+func home(w http.ResponseWriter, r *http.Request) {
+	homeTemplate.Execute(w, "ws://"+r.Host+"/echo") //websocket endpoint
+}
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/", home)
+	chat_log = append(chat_log, "hi")
+	chat_log = append(chat_log, "hello")
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
+
+//ws = new WebSocket("{{.}}"); => "ws://"+r.Host+"/echo"
+
+var homeTemplate = template.Must(template.New("").Parse(`
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<script>  
+window.addEventListener("load", function(evt) {
+    var output = document.getElementById("output");
+    var input = document.getElementById("input");
+    var ws;
+    var print = function(message) {
+        var d = document.createElement("div");
+        d.textContent = message;
+        output.appendChild(d);
+        output.scroll(0, output.scrollHeight);
+    };
+    document.getElementById("open").onclick = function(evt) {
+        if (ws) {
+            return false;
+        }
+        ws = new WebSocket("{{.}}");
+        ws.onopen = function(evt) {
+            print("OPEN");
+        }
+        ws.onclose = function(evt) {
+            print("CLOSE");
+            ws = null;
+        }
+        ws.onmessage = function(evt) {
+            print("RESPONSE: " + evt.data);
+        }
+        ws.onerror = function(evt) {
+            print("ERROR: " + evt.data);
+        }
+        return false;
+    };
+    document.getElementById("send").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        print("SEND: " + input.value);
+        ws.send(input.value);
+        return false;
+    };
+    document.getElementById("close").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        ws.close();
+        return false;
+    };
+});
+</script>
+</head>
+<body>
+<table>
+<tr><td valign="top" width="50%">
+<p>Click "Open" to create a connection to the server, 
+"Send" to send a message to the server and "Close" to close the connection. 
+You can change the message and send multiple times.
+<p>
+<form>
+<button id="open">Open</button>
+<button id="close">Close</button>
+<p><input id="input" type="text" value="Hello world!">
+<button id="send">Send</button>
+</form>
+</td><td valign="top" width="50%">
+<div id="output" style="max-height: 70vh;overflow-y: scroll;"></div>
+</td></tr></table>
+</body>
+</html>
+`))
